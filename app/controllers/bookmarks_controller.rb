bd3dd8030dd41b67d579ce964235ce9fb972f3e2
Damian Hallengren <djhallengren@gmail.com>
2023-02-24 12:30:47 +0100

    Looks great... totally

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    bookmark = Bookmark.new(bookmark_params)
+    if bookmark.save
+      redirect_to list_path(bookmark.list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    bookmark = Bookmark.find(params[:id])
+    list = bookmark.list
+    bookmark.destroy
+    redirect_to list_path(list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(list: @list)
   end
 
   def new
